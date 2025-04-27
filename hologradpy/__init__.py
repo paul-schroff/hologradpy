d9b5ef73ffedbb3c97dbdb68ca22961bcd474fa8
paul-schroff <paul.schroff@strath.ac.uk>
2025-04-27 16:47:35 +0100

    Added __init__.py to subfolders.

--- hologradpy/__init__.py
+++ hologradpy/__init__.py
@@ -1,6 +1,17 @@
-from . import calibrate_slm
-from . import error_metrics
-from . import fitting
+from .calibration import calibrate_slm
+from .analysis import error_metrics, fitting
+from .holography import holography, phase_retrieval, loss_functions
+from .torch_modules import (
+    propagators,
+    planar_modules,
+    optical_systems,
+    pixel_crosstalk,
+)
+from .torch_modules.utils import (
+    tensor_utils,
+    optics_utils,
+    fourier_utils,
+)
 from . import hardware
 from . import patterns
 from . import torch_functions
