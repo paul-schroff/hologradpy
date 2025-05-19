948dbfc47602c3164f1ec4150c682e8c4440e3b2
paul-schroff <paul.schroff@strath.ac.uk>
2025-05-19 22:48:09 +0100

    Added linear_phase() and sanitized name of rectangular_mask().

--- hologradpy/torch_modules/utils/optics_utils.py
+++ hologradpy/torch_modules/utils/optics_utils.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+from typing import Literal
 import torch
+
 
 def lens_phase(
         x: torch.Tensor,
@@ -8,6 +10,57 @@ def lens_phase(
         wavenumber:float
     ) -> torch.Tensor:
     return -0.5 * wavenumber / focal_length * (x ** 2 + y ** 2)
+
+# TODO: Sanity check this function
+def linear_phase(
+        x: torch.Tensor,
+        y: torch.Tensor,
+        tilt_x: float,
+        tilt_y: float,
+        tilt_units: Literal[
+            'degrees',
+            'radians',
+            'metres',
+            'lines_per_mm',
+            ] = 'metres',
+        wavenumber: float | None = None,
+        focal_length: float | None = None,
+    ) -> torch.Tensor:
+    match tilt_units:
+        case 'degrees':
+            slope_x = torch.tan(tilt_x) * wavenumber
+            slope_y = torch.tan(tilt_y) * wavenumber
+        case 'radians':
+            slope_x = tilt_x * wavenumber
+            slope_y = tilt_y * wavenumber
+        case 'metres':
+            if focal_length is None:
+                raise ValueError(
+                    'Focal length must be provided when tilt_units is '
+                    '"metres".'
+                )
+            slope_x = tilt_x / focal_length
+            slope_y = tilt_y / focal_length
+        case 'lines_per_mm':
+            raise NotImplementedError('lines_per_mm not implemented yet')
+    return slope_x * x + slope_y * y
+
+# TODO: Sanity check this function
+def quadratic_phase(
+        x: torch.Tensor,
+        y: torch.Tensor,
+        curvature: float,
+        aspect_ratio: float = 1.0,
+        curvature_units: Literal[
+            'radians_per_pixel_squared',
+            'radians_per_metre_squared'] = 'radians_per_metre_squared',
+    ) -> torch.Tensor:
+    if curvature_units == 'radians_per_pixel_squared':
+        x = x / (x.max() - x.min()) * x.shape[1]
+        y = y / (y.max() - y.min()) * y.shape[0]
+    return (
+        4 * curvature * (aspect_ratio * y ** 2 + (1 - aspect_ratio) * x ** 2)
+    )
 
 def gaussian_beam_intensity(
         x: torch.Tensor,
@@ -24,7 +77,7 @@ def gaussian_beam_intensity(
         ) + offset
 
 # %% Binary aperture functions
-def rect_mask(
+def rectangular_mask(
         x: torch.Tensor, 
         y: torch.Tensor,
         width: float, 
