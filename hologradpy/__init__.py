66e7ef81d08041c2572b7afb8b4c871e24514e08
paul-schroff <paul.schroff@strath.ac.uk>
2025-07-06 10:42:18 +0100

    Updated imports.

--- hologradpy/__init__.py
+++ hologradpy/__init__.py
@@ -1,13 +1,13 @@
 from .calibration import calibrate_slm
 from .analysis import error_metrics, fitting
 from .holography import phase_retrieval, loss_functions, camera_feedback
-from .torch_modules import (
+from .propagation import (
     elements,
     propagators,
     optical_systems,
     pixel_crosstalk,
 )
-from .torch_modules.utils import (
+from .propagation.utils import (
     tensor_utils,
     optics_utils,
     fourier_utils,
--- hologradpy/hardware/__init__.py
+++ hologradpy/hardware/__init__.py
@@ -1,12 +1,7 @@
-import slmsuite.hardware.slms as slms
-import slmsuite.hardware.cameras as cameras
-
 from .torch_slm import SimulatedSLMTorch
 from .torch_camera import SimulatedCameraTorch
 
 __all__ = [
-    "slms",
-    "cameras",
     "SimulatedSLMTorch",
     "SimulatedCameraTorch",
 ]
