ad08f17ef8196a8ead64fe5dc50ac6b0961a0954
paul-schroff <paul.schroff@strath.ac.uk>
2025-07-06 10:40:37 +0100

    Added crop_to_roi() and find_roi(). Using array_api_compat package for backend-agnostic functions.

--- hologradpy/propagation/utils/tensor_utils.py
+++ hologradpy/propagation/utils/tensor_utils.py
@@ -1,6 +1,12 @@
+from typing import TypeVar
 import torch
 import numpy as np
 from numpy.typing import NDArray
+
+from array_api_compat import array_namespace
+
+ArrayLike = TypeVar("ArrayLike", torch.Tensor, NDArray)
+
 
 def check_device(verbose: bool = False) -> str:
     """
@@ -14,16 +20,20 @@ def check_device(verbose: bool = False) -> str:
         print(f'Using {device} device')
     return device
 
+
 def gpu_to_numpy(tensor: torch.Tensor) -> NDArray:
     return tensor.clone().cpu().detach().numpy()
+
 
 def unsqueeze_to(
         input: torch.Tensor,
         max_dim: int,
         dim: int = 0
     ) -> torch.Tensor:
-    while input.dim() < max_dim: input = input.unsqueeze(dim)
+    while input.dim() < max_dim:
+        input = input.unsqueeze(dim)
     return input
+
 
 def pad_to_shape_2D(input: torch.Tensor,
                     target_shape: tuple[int, int]
@@ -43,10 +53,11 @@ def pad_to_shape_2D(input: torch.Tensor,
         pad = (pad_w, pad_w, pad_h, pad_h)
         return torch.nn.functional.pad(input, pad)
 
+
 def crop_to_shape_2D(
-        input: torch.Tensor | NDArray,
+        input: ArrayLike,
         target_shape: tuple[int, int]
-    ) -> torch.Tensor | NDArray:
+    ) -> ArrayLike:
     input_shape = input.shape[-2:]
     crop_h = (input_shape[0] - target_shape[0]) // 2
     crop_w = (input_shape[1] - target_shape[1]) // 2
@@ -54,9 +65,50 @@ def crop_to_shape_2D(
                  crop_h:input_shape[0] - crop_h,
                  crop_w:input_shape[1] - crop_w]
 
+
 def crop_to_roi(
-        input: torch.Tensor | NDArray,
-        roi: tuple[int, int, int, int]
-    ) -> torch.Tensor | NDArray:
-    x, width, y, height = roi
-    return input[..., y:y + height, x:x + width]
+    input: ArrayLike,
+    roi: tuple[int, int, int, int],
+) -> ArrayLike:
+    """Crops the input image to the specified region of interest.
+
+    Args:
+        input (ArrayLike): The 2D data to be cropped.
+        roi (tuple[int, int, int, int]): The region of interest to be cropped,
+            as a tuple of (top, bottom, left, right) pixel indices.
+
+    Returns:
+        ArrayLike: The cropped image.
+    """
+    return input[..., roi[0] : roi[1], roi[2] : roi[3]]
+
+
+def find_roi(
+    input: ArrayLike, threshold: float = 0.5, pad: int = 10
+) -> tuple[int, int, int, int]:
+    """Finds the rectangular region of interest in an image including relative
+    pixel values larger than threshold. ROI can be padded symmetrically on
+    each side using pad.
+
+    Args:
+        input (ArrayLike): The image data in which to find the
+            region of interest.
+        threshold (float, optional): The fraction of the maximum pixel value
+            at which pixels must be include in the region of interest.
+            Defaults to 0.5.
+        pad (int, optional): The number of pixels to pad on both axes around
+            the threshold values. Defaults to 10.
+
+    Returns:
+        tuple[int, int, int, int]: The region of interest, formatted as (top,
+            bottom, left, right) pixel indices.
+    """
+    xp = array_namespace(input)
+    idx_y, idx_x = xp.where(input > threshold * xp.max(input))
+
+    max_idx_y = int(xp.clip(xp.max(idx_y) + pad, 0, input.shape[0]))
+    min_idx_y = int(xp.clip(xp.min(idx_y) - pad, 0, input.shape[0]))
+    max_idx_x = int(xp.clip(xp.max(idx_x) + pad, 0, input.shape[1]))
+    min_idx_x = int(xp.clip(xp.min(idx_x) - pad, 0, input.shape[1]))
+
+    return (min_idx_y, max_idx_y, min_idx_x, max_idx_x)
