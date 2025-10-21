f81aecaac48bfb841b8e1ca1c1d45ef3c7d75207
paul-schroff <paul.schroff@strath.ac.uk>
2025-10-21 20:34:17 +0100

    Updated imports.

--- hologradpy/propagation/propagators/__init__.py
+++ hologradpy/propagation/propagators/__init__.py
@@ -1,11 +1,11 @@
 from .abstract import PropagatorBase
 from .angular_spectrum_method import AngularSpectrumMethod
-from .fourier_lens_fft import FourierLensFft
+from .fourier_lens_fft import FourierLensFFT
 from .fourier_lens_nufft import FourierLensNufft
 
 __all__ = [
     "PropagatorBase",
     "AngularSpectrumMethod",
-    "FourierLensFft",
+    "FourierLensFFT",
     "FourierLensNufft"
 ]
