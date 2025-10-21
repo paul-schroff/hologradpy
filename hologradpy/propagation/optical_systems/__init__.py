f2961242d6a7dfe1111d668d19e8236d95c030e8
paul-schroff <paul.schroff@strath.ac.uk>
2025-10-21 20:48:05 +0100

    Updated imports.

--- hologradpy/propagation/optical_systems/__init__.py
+++ hologradpy/propagation/optical_systems/__init__.py
@@ -1,9 +1,11 @@
 from .abstract import SLMCameraModel
+from .slm_fft import SLMFFT
 from .slm_fft_affine import SLMFFTAffine
 from .slm_nufft_affine import SLMNUFFTAffine
 
 __all__ = [
     "SLMCameraModel",
+    "SLMFFT",
     "SLMFFTAffine",
     "SLMNUFFTAffine",
 ]
