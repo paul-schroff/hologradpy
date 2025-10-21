ad61a7a99fafe1fd9f91960a2fc43caf1171fb6a
paul-schroff <paul.schroff@strath.ac.uk>
2025-10-21 20:52:16 +0100

    Updated class name to PhaseRetrieverBase. Added parameter_name input argument in PhaseRetrieverBase.set_gradient_requirements().

--- hologradpy/holography/phase_retrieval/abstract.py
+++ hologradpy/holography/phase_retrieval/abstract.py
@@ -5,21 +5,25 @@ from numpy.typing import NDArray
 
 from ...propagation.optical_systems import SLMCameraModel
 
-class PhaseRetrievalBase:
+
+class PhaseRetrieverBase:
     def __init__(
-        self,
-        slm_camera_model: SLMCameraModel,
-        device: str = "cpu",
-    ) -> None:
+            self,
+            slm_camera_model: SLMCameraModel,
+            device: str = "cpu",
+        ) -> None:
         self.slm_camera_model: SLMCameraModel = slm_camera_model
         self.device: str = device
 
     def set_optimizer(self):
         pass
 
-    def set_gradient_requirements(self) -> None:
+    def set_gradient_requirements(
+            self,
+            parameter_name: str = "virtual_slm.phase"
+        ) -> None:
         for name, parameter in self.slm_camera_model.named_parameters():
-            if name == "virtual_slm.phase":
+            if name == parameter_name:
                 parameter.requires_grad = True
             else:
                 parameter.requires_grad = False
