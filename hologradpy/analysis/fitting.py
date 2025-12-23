194737d045011b6021d25aa645d47a97384f299f
paul-schroff <paul.schroff@strath.ac.uk>
2025-12-23 14:39:16 +0100

    Formatting improvements.

--- hologradpy/analysis/fitting.py
+++ hologradpy/analysis/fitting.py
@@ -1,6 +1,7 @@
 """
 This module contains functions for curve fitting.
 """
+
 from typing import Callable, Sequence
 
 import numpy as np
@@ -14,28 +15,26 @@ from .functions import tilt, interferometric_fringes
 
 from ..propagation.utils.optics_utils import gaussian_beam_intensity
 
+
 def curve_fit_2d(
-        x: NDArray,
-        y: NDArray,
-        data: NDArray,
-        func: Callable[[NDArray, NDArray, Sequence[float]], NDArray],
-        *args,
-        **kwargs
-    ) -> tuple[NDArray, NDArray]:
+    x: NDArray,
+    y: NDArray,
+    data: NDArray,
+    func: Callable[[NDArray, NDArray, Sequence[float]], NDArray],
+    *args,
+    **kwargs,
+) -> tuple[NDArray, NDArray]:
     x_data = np.vstack((x.ravel(), y.ravel()))
 
     def func_flat(x_data_flat, *params):
         x_flat, y_flat = x_data_flat
         return func(x_flat, y_flat, *params)
-    
+
     popt, pcov = curve_fit(func_flat, x_data, data.ravel(), *args, **kwargs)
     return popt, pcov
 
 
-def remove_tilt(
-    img: NDArray,
-    mask: NDArray | None = None
-) -> NDArray:
+def remove_tilt(img: NDArray, mask: NDArray | None = None) -> NDArray:
     """
     This function removes fits the first three Zernike polynomials (Piston and
     tilt) to an image and subtracts the
@@ -60,17 +59,17 @@ def remove_tilt(
 
 
 def fit_gaussian_beam_intensity(
-        x: NDArray,
-        y: NDArray,
-        data: NDArray,
-        beam_radius_guess: float,
-        blur_sigma: float = 10
-        ) -> tuple[NDArray[np.float_], NDArray[np.float_]]:
-    
+    x: NDArray,
+    y: NDArray,
+    data: NDArray,
+    beam_radius_guess: float,
+    blur_sigma: float = 10,
+) -> tuple[NDArray[np.float_], NDArray[np.float_]]:
+
     data_blurred = gaussian_filter(data, blur_sigma)
     index = np.unravel_index(np.argmax(data_blurred), data.shape)
     p0 = (beam_radius_guess, x[index], y[index], np.max(data), 0)
-    
+
     popt, pcov = curve_fit_2d(
         x,
         y,
@@ -82,18 +81,18 @@ def fit_gaussian_beam_intensity(
 
 
 def fit_interferometric_fringes(
-        x: NDArray,
-        y: NDArray,
-        data: NDArray,
-        separation_x: float,
-        separation_y: float,
-        wavenumber: float,
-        focal_length: float,
-        phase_guess: float = 0,
-        amplitude_guess: float = 1,
-        max_iterations: int = 10000,
-        ) -> tuple[NDArray[np.float_], NDArray[np.float_]]:
-    """Fit the interference pattern on the camera caused by two superpixels 
+    x: NDArray,
+    y: NDArray,
+    data: NDArray,
+    separation_x: float,
+    separation_y: float,
+    wavenumber: float,
+    focal_length: float,
+    phase_guess: float = 0,
+    amplitude_guess: float = 1,
+    max_iterations: int = 10000,
+) -> tuple[NDArray[np.float_], NDArray[np.float_]]:
+    """Fit the interference pattern on the camera caused by two superpixels
     on the SLM separated by separation_x and separation_y. Equation adapted
     from https://doi.org/10.1364/OE.24.013881.
     Args:
@@ -104,14 +103,15 @@ def fit_interferometric_fringes(
         separation_y (float): Separation between two superpixels along y.
         wavenumber (float): Wavenumber of the light.
         focal_length (float): Focal length of the Fourier lens.
-        phase_guess (float, optional): Phase difference between the two 
+        phase_guess (float, optional): Phase difference between the two
             superpixels. Defaults to 0.
-        amplitude_guess (float, optional): Amplitude due to superpixel a. 
+        amplitude_guess (float, optional): Amplitude due to superpixel a.
             Defaults to 1.
     Returns:
         tuple[NDArray[np.float_], NDArray[np.float_]]: Fitting parameters
             (phase, amplitude) and covariance matrix.
     """
+
     def fit_function(x_, y_, phase, amplitude):
         return interferometric_fringes(
             x_,
@@ -121,18 +121,12 @@ def fit_interferometric_fringes(
             wavenumber,
             focal_length,
             phase,
-            amplitude
+            amplitude,
         )
+
     p0 = (phase_guess, amplitude_guess)
     bounds = ((-np.pi, 0), (np.pi, np.inf))
     popt, pcov = curve_fit_2d(
-        x,
-        y,
-        data,
-        fit_function,
-        p0=p0, 
-        bounds=bounds,
-        maxfev=max_iterations
+        x, y, data, fit_function, p0=p0, bounds=bounds, maxfev=max_iterations
     )
     return popt, pcov
-
